c4053dc9a15edf90c5cf1922d6a50fa4aef1bc51
Heeseok-Jeong <dldydldy@naver.com>
2018-03-29 17:05:14 +0900

    add instance variable

--- src/main/java/edu/handong/csee/java/lab06/BugInfestation.java
+++ src/main/java/edu/handong/csee/java/lab06/BugInfestation.java
@@ -3,8 +3,11 @@ import java.util.Scanner;
 
 public class BugInfestation {
 
+	double GROWTH_RATE = 0.95;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		 
 
 	}
 
