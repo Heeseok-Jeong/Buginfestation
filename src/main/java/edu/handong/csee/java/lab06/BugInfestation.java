5c1ad5f6b96e29676b29b28ba42d1ee021b13f08
Heeseok-Jeong <dldydldy@naver.com>
2018-03-30 02:43:45 +0900

    Complete the program

--- src/main/java/edu/handong/csee/java/lab06/BugInfestation.java
+++ src/main/java/edu/handong/csee/java/lab06/BugInfestation.java
@@ -12,57 +12,57 @@ public class BugInfestation { //make a class 'BugInfestation'
 	final double GROWTH_RATE = 0.95; //initialize instance variable, using final keyword for make a constant
 	final double ONE_BUG_VOLUME = 0.002; //initialize instance variable, using final keyword for make a constant
 	
-	double hVolume; //declare instance variable
-	int sPopulation; //declare instance variable
+	double houseVolume; //declare instance variable
+	double totalBugVolume; //declare instance variable
+	double startPopulation; //declare instance variable
+	double population;  //declare instance variable
+	double newBugs; //declare instance variable
+	double newBugVolume; //declare instance variable
+	int countWeeks = 0; //initialize instance variable
 	
 	/**
 	 * main method do a practical running 
 	 */
 	public static void main(String[] args) { //make a main method
-		double houseVolume; //declare a local variable
-		int startPopulation; //declare a local variable
-		BugInfestation buginf = new BugInfestation(); //instantiate buginf
-		Scanner keyboard = new Scanner(System.in); //instantiate keyboard
-		
-		
+		double hVolume; //declare a local variable
+		double sPopulation; //declare a local variable
+		BugInfestation bugInf = new BugInfestation(); //instantiate bugInf
+		Scanner keyboard = new Scanner(System.in); //instantiate keyboard	
 		
 		System.out.println("Enter the total volume of your house"); //print some message
 		System.out.println("in cubic feet : "); //print some message
-		houseVolume = keyboard.nextDouble(); //get a house volume value from user
+		hVolume = keyboard.nextDouble(); //get a house volume value from user
 		System.out.println("Enter the estimated number of "); //print some message
 		System.out.println("roaches in your house : "); //print some message
-		startPopulation = keyboard.nextInt(); //get a start population value from user
-		System.out.println("Starting with a roach population of " + startPopulation); //print about start population
-		System.out.println("and a house with a volume of " + houseVolume + " cubic feet,"); //print about house volume
+		sPopulation = keyboard.nextDouble(); //get a start population value from user
+		System.out.println("Starting with a roach population of " + (int)sPopulation); //print about start population
+		System.out.println("and a house with a volume of " + hVolume + " cubic feet,"); //print about house volume
 		
-		buginf.hVolume = houseVolume; //hVolume's value is houseVolumes's value
-		buginf.sPopulation = startPopulation; //sPopulation's value is startPopulation's value
+		bugInf.houseVolume = hVolume; //houseVolume's value is hVolumes's value
+		bugInf.startPopulation = sPopulation; //startPopulation's value is sPopulation's value
+		bugInf.computeAllThing(); //call a method to compute all variables 
 		
-		System.out.println("after " + buginf.computeWeek() + " weeks,"); //print about how many weeks 
-		System.out.println("the house will be filled with " + buginf.computeBugPopulation() + " roaches."); //print about how many bugs
-		System.out.println("They will fill a volume of " + buginf.computeBugVolume() + " cubic feet"); //print about how much bugs volume
+		System.out.println("after " + bugInf.countWeeks + " weeks,"); //print about how many weeks 
+		System.out.println("the house will be filled with " + (int)bugInf.population + " roaches."); //print about how many bugs
+		System.out.println("They will fill a volume of " + (int)bugInf.totalBugVolume + " cubic feet"); //print about how much bugs volume
 		System.out.println("Better call Debugging Experts Inc."); //print some message
 		 
 	}
 	
 	/**
-	 * this method computes how long weeks be taken and how many bugs are in a house 
+	 * this method computes totalBugVolume, countWeeks and population
 	 */
-	public int computeWeek() { //make a method 
-	
-	}
-	
-	/**
-	 * this method computes how many bugs are in a house 
-	 */
-	public int computeBugPopulation() { //make a method
+	public void computeAllThing() { //make a method
+		population = startPopulation; //define what population's first value 
+		totalBugVolume = population * ONE_BUG_VOLUME; //define what totalBugVolume's first value
 		
+		while(totalBugVolume < houseVolume) { //looping when totalBugVolume isn't small than houseVolume
+			newBugs = population * GROWTH_RATE; //define newBugs's value
+			newBugVolume = newBugs * ONE_BUG_VOLUME; //define newBugVolume's value
+			population += newBugs; //define population's value
+			totalBugVolume += newBugVolume; //define totalBugVolume's value
+			countWeeks ++; //plus 1 to countWeeks when next line
+		}
 	}
 	
-	/**
-	 * this method computes how much bug's volume is 
-	 */
-	public double computeBugVolume() { //make a method
-		
-	}
 }
